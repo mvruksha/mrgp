df689366d2f77b2d0a15ad6568df29c7ddb014d7
raghavsrirama <raghav.srirama@gmai.com>
2025-06-02 22:58:14 +0530

    Updated grid-cols-1 in RealEstatePagination.jsx

--- src/components/residentialProPage/RealEstatePagination.jsx
+++ src/components/residentialProPage/RealEstatePagination.jsx
@@ -81,7 +81,7 @@ const RealEstatePagination = ({ properties }) => {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {currentProperties.map((property, index) => (
             <div
               key={index}
