a15f3eacc4ce1e45b2f5f86e41f2d1e215bf950d
raghavsrirama <raghav.srirama@gmai.com>
2025-06-05 19:34:23 +0530

    mp4 try

--- src/data/residential-properties/DataResidential.jsx
+++ src/data/residential-properties/DataResidential.jsx
@@ -83,7 +83,7 @@ const properties = [
     images: [
       "/assets/home/properties/5.jpeg",
       "/assets/home/properties/6.jpeg",
-      "/assets/home/properties/5.jpeg",
+      "/assets/home/properties/12.mp4",
     ],
     price: 0,
     bedrooms: null,
